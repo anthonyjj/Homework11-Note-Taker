b53b6a9a3b06f8b6e136686dcc9fd3e723925151
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-05-02 15:40:28 -0700

    imported pages.js into server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {pageRouter} = require('./routes/pages');
 
 const app = express();
 
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true} ))
+app.use('/', pageRouter);
 
 app.listen(PORT, ()=>{
     console.info(`Server started on http://localhost:${PORT}`)
