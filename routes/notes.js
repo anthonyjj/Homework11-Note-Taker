09d9eca154b0beb4ce1bedfeb7e221d6d7bc7102
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-05-08 23:24:52 -0700

    added premade tasks to note taker

---
+++ routes/notes.js
@@ -0,0 +1,12 @@
+const pageRouter = require ('express').Router();
+const path = require ('path');
+
+const premadeTasks = [
+    "Note 1: Call bank on Firday",
+    "Note 2: Reschedule delivery",
+    "Note 3: Finish Payroll",
+    "Note 4: Check in with clients",
+    "Note 5: Client lunch on Thursday",
+    "Note 6: Make dinner",
+];
+
